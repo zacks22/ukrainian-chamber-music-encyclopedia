c6cadc7f9a0fcb891ddde99b6209b6a0a2271361
Matts52 <32623708+Matts52@users.noreply.github.com>
2024-12-27 21:14:03 -0600

    Updates

---
+++ csv_to_json.py
@@ -0,0 +1,31 @@
+import csv
+import json
+
+def make_json(csvFilePath, jsonFilePath):
+    # Create a list to hold rows
+    data = []
+    
+    # Open a CSV reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+        
+        # Append each row as a dictionary to the list
+        for rows in csvReader:
+            data.append(rows)
+
+    # Open a JSON writer, and use the json.dumps() 
+    # function to dump data
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+        
+# Driver Code
+
+# Define the file paths according to your system
+
+file_name = 'test_composers'
+
+csvFilePath = f'/Users/matthewsenick/Github/ukrainian-chamber-music-encyclopedia/public/{file_name}.csv'
+jsonFilePath = f'/Users/matthewsenick/Github/ukrainian-chamber-music-encyclopedia/public/{file_name}.json'
+
+# Call the make_json function
+make_json(csvFilePath, jsonFilePath)
