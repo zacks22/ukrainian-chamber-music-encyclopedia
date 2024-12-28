dc177e1f2b740848fae965b7d81204154592bc0a
Matts52 <32623708+Matts52@users.noreply.github.com>
2024-12-28 11:16:39 -0600

    create a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -10,64 +10,70 @@ import DifficultyList from './components/DifficultyList';
 import DifficultyInfo from './components/DifficultyInfo';
 import PieceLengthList from './components/PieceLengthList';
 import PieceLengthInfo from './components/PieceLengthInfo';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
-    <Router basename={import.meta.env.BASE_URL}>
-      <Routes>
-        {/* Main Page */}
-        <Route
-          path="/"
-          element={
-            <div className="wrapper">
-              <h1>Ukrainian Chamber Music Encyclopedia</h1>
-              <h2>
-                <Link to={`/composers`}>Composers</Link>
-              </h2>
-              <h2>
-                <Link to={`/instrumentation_category`}>Instrumentation Category</Link>
-              </h2>
-              <h2>
-                <Link to={`/difficulty_levels`}>Difficulty</Link>
-              </h2>
-              <h2>
-                <Link to={`/piece_lengths`}>Piece Length</Link>
-              </h2>
-              <p>Here is a wonderful description of this Ukrainian chamber music encyclopedia</p>
-            </div>
-          }
-        />
+    <>
+      <Navbar></Navbar>
 
-        {/* Composer List Page */}
-        <Route path="/composers" element={<ComposerList />} />
+      <Router basename={import.meta.env.BASE_URL}>
+        <Routes>
+          {/* Main Page */}
+          <Route
+            path="/"
+            element={
+              <div className="wrapper">
+                <h1>Ukrainian Chamber Music Encyclopedia</h1>
+                <h2>
+                  <Link to={`/composers`}>Composers</Link>
+                </h2>
+                <h2>
+                  <Link to={`/instrumentation_category`}>Instrumentation Category</Link>
+                </h2>
+                <h2>
+                  <Link to={`/difficulty_levels`}>Difficulty</Link>
+                </h2>
+                <h2>
+                  <Link to={`/piece_lengths`}>Piece Length</Link>
+                </h2>
+                <p>Here is a wonderful description of this Ukrainian chamber music encyclopedia</p>
+              </div>
+            }
+          />
 
-        {/* Difficulty List Page */}
-        <Route path="/difficulty_levels" element={<DifficultyList />} />
+          {/* Composer List Page */}
+          <Route path="/composers" element={<ComposerList />} />
 
-        {/* Piece Length List Page */}
-        <Route path="/piece_lengths" element={<PieceLengthList />} />
+          {/* Difficulty List Page */}
+          <Route path="/difficulty_levels" element={<DifficultyList />} />
 
-        {/* Instrumentation Category List Page */}
-        <Route path="/instrumentation_category" element={<InstrumentationCategoryList />} />
+          {/* Piece Length List Page */}
+          <Route path="/piece_lengths" element={<PieceLengthList />} />
 
-        {/* Composer Info Page */}
-        <Route path="/composer/:name" element={<ComposerInfo />} />
+          {/* Instrumentation Category List Page */}
+          <Route path="/instrumentation_category" element={<InstrumentationCategoryList />} />
 
-        {/* Instrumentation Category Info Page */}
-        <Route path="/instrumentation_category/:category" element={<InstrumentationCategoryInfo />} />
+          {/* Composer Info Page */}
+          <Route path="/composer/:name" element={<ComposerInfo />} />
 
-        {/* Difficulty Info Page */}
-        <Route path="/difficulty_levels/:difficulty" element={<DifficultyInfo />} />
+          {/* Instrumentation Category Info Page */}
+          <Route path="/instrumentation_category/:category" element={<InstrumentationCategoryInfo />} />
 
-        {/* Piece Length Info Page */}
-        <Route path="/piece_lengths/:length" element={<PieceLengthInfo />} />
+          {/* Difficulty Info Page */}
+          <Route path="/difficulty_levels/:difficulty" element={<DifficultyInfo />} />
 
-        {/* Piece Info Page with composer and title as parameters */}
-        <Route path="/piece/:composer/:title" element={<PieceInfo />} />
+          {/* Piece Length Info Page */}
+          <Route path="/piece_lengths/:length" element={<PieceLengthInfo />} />
 
-      </Routes>
-    </Router>
+          {/* Piece Info Page with composer and title as parameters */}
+          <Route path="/piece/:composer/:title" element={<PieceInfo />} />
+
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import './Navbar.css'; // Optional: For styling if needed
+
+const Navbar = () => {
+    return (
+        <header>
+            <nav className="navbar">
+                <ul className="navbar-list">
+                    <div className="navbar-item">
+                        <a href="/">
+                            <img src='./icons/icons8-home.svg' alt="Home" className="navbar-icon" />
+                        </a>
+                    </div>
+                </ul>
+                <div className="navbar-title">Ukrainian Chamber Music Encyclopedia</div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
